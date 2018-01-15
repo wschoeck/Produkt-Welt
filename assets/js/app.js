826f6585e43b43fd62a444b940e0e2a9b65502ca
Wolfgang Schoeck <wschoeck@bluewin.ch>
2018-01-15 09:20:34 +0100

    Auf Basis des Viewport-Skeletons erstelle Datei

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,28 @@
-$(document).foundation()
+$(document).foundation();
+
+
+/*
+-- mit each wird durch jede section gegangen
+-- für jede wird onScreen initialisiert
+-- onScreen prüft ob das Element im viewport sichtbar wird
+-- falls sichtbar wird die Klasse isonScreen zugefügt
+-- falls das Element wieder aus dem viewport verschwindet, wird die Klasse wieder entfernt
+-- mit dieser Klasse kann im css gearbeitet werden, siehe ab Zeile 22 im app.css
+*/
+
+$( "section" ).each(function( index, section ) {
+     $(section).onScreen({
+        container: window,
+        direction: 'vertical',
+        doIn: function() {
+            $(section).addClass('onScreen');
+        },
+        doOut: function() {
+            $(section).removeClass('onScreen');
+        },
+        tolerance: 0,
+        throttle: 50,
+        lazyAttr: null,
+        debug: false
+    });
+});
